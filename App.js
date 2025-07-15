04fbdd2e1015abe25613e32c9f8b592e0a0c4bb5
venkatesh <venkatesh.musham56@gmail.com>
2025-07-15 12:08:32 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,27 @@
+// const heading = React.createElement("h1", {id : "heading"}, "this is h1 tag");
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    [
+        React.createElement(
+            "div", 
+            {id:"child", key:"child", className: "child-class"}, 
+            [
+                React.createElement("h1", {id:"heading", key:"heading"}, "this is child"), 
+                React.createElement("h2", {id:"sub-heading", key:"sub-heading"}, "this is sub child")
+            ]
+        ),
+        React.createElement(
+            "div", 
+            {id:"child2", key:"child2"}, 
+            [
+                React.createElement("h1", {id:"heading", key:"heading"}, "this is child"), 
+                React.createElement("h2", {id:"sub-heading", key:"sub-heading"}, "this is sub child")
+            ]
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
