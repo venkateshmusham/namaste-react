84c1daf584b3519ea4f27fdc838c034200bc4cc7
venkatesh <venkatesh.musham56@gmail.com>
2025-07-22 05:04:26 +0530

    routes start

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { Footer } from "./components/Footer"; // it is mentioned as name export in Footer.js file
-
+// routes
 const App = () => {
     return (
         <div className="app">
